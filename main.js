4f7ce915b89ea9bd79f712d3416973247699d28b
Randomspyguy <humphrey.zak@outlook.com>
2022-08-14 10:40:18 -0600

    Hello World

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 
+    $(".body-border").css({'bottom' : 0});
+
     $('#about, #portfolio, #projects, #contact').click(function(){
         $(".title").addClass("animate-5vw-up");
-    }) 
+        $(".pic").delay(2500).removeClass('hide').addClass('animate__delay-1s').addClass("animate__fadeIn");
+        $("#about_ln1").removeClass('hide').addClass('animate__delay-1s').addClass('animate__fadeInUp');
+        $("#about_ln2").removeClass('hide').addClass('animate__delay-2s').addClass('animate__fadeInUp');
+    });
 
     $('#home').click(function(){
         $("#home").addClass('success').removeClass('warning');
@@ -11,10 +16,11 @@ $(document).ready(function(){
         $("#projects").addClass('warning').removeClass('alert');
         $("#contact").addClass('warning').removeClass('alert');
         $("h1").css({'color': '#CDD3AC'});
-    })
+    });
     
     
     $('#about').click(function(){
+        $(".body-border").css({'bottom' : '', 'margin-bottom' : 'auto'});
         $("#about").addClass('success').removeClass('warning');
         $("#home").addClass('warning').removeClass('alert');
         $("#portfolio").addClass('warning').removeClass('alert');
